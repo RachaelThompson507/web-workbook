240eccc24224f41548e7a9b691161d3d8841d06f
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-08-13 23:12:56 -0500

    init commit

---
+++ 06week/javascripting/if-statement.js
@@ -0,0 +1,14 @@
+var fruit = 'orange';
+if (fruit.length > 5) {
+  console.log("The fruit name has more than five characters.");
+} else {
+  console.log("The fruit name has five characters or less." );
+}
+
+
+// var fruit = 'Orange';
+// if (fruit.length > 5){
+//   console.log(fruit + ' has more than 5 characters!');
+// } else {
+//   console.log(fruit + ' has less than 5 characters!');
+// }
