d61e4fe8138058b5cede035e59f5d8848b9739b5
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-08-23 18:46:13 -0500

    init

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
+// document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
-});
+window.onload = function() {
+  // var counts = document.getElementsByTagName('ul');
+  // console.log(count[0].children.lengh);
+  // let count = list[0].children.length;
+  // console.log(count);
+  var count = document.querySelectorAll('li');
+  console.log(count.length);
+
+
+// var newh2 = document.createElement('h2');
+// newh2.innerHTML="You have"+list[0].children.length + "items in your shopping cart";
+// document.h1.appendChild(newh2);
+
+//document.querySelector('h1').insertAdjacentHTML('afterEnd', "<h2> You have" + count.length + "items in your shopping cart</h2>");
+var newh2 = document.createElement('h2');
+newh2.innerHTML = 'you have ' + count.length + ' items in your shopping cart';;
+document.getElementsByTagName('h1')[0].append(newh2);
+
+}
+
+
+//});
