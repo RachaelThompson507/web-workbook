57092b7bcdf60ee6cf8a5066258514af85a89968
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-08-26 17:43:22 -0500

    THANKS DIHN

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,40 +1,65 @@
 'use strict';
-document.addEventListener("DOMContentLoaded", function(event) {
 
 window.onload = function() {
-//item count
-  var itemCount = document.querySelectorAll('li');
-  console.log(itemCount.length);
-//h2
+  updateCounter();
+  insertText();
+}
+
+//create items from input text
+function insertText(){
+  var addNewItems = document.createElement('input');
+  addNewItems.setAttribute('type', 'text');
+  addNewItems.setAttribute('placeholder', 'Type in an Item');
+  var list = document.getElementsByTagName('ul');
+  list[0].after(addNewItems);
+  addNewItems.setAttribute('id', 'input-text');
+
+  var buttonAdd= document.createElement('button');
+  buttonAdd.innerHTML = 'add item';
+  buttonAdd.setAttribute('id', 'addButton');
+  var list1 = document.getElementsByTagName('input');
+  list1[0].after(buttonAdd);
+  buttonAdd.onclick= addItem;
+}
+
+//add items to cart
+function addItem(event) {
+  var newItemText = document.getElementById('input-text');
+  var newList = document.createElement('li');
+  newList.innerHTML= newItemText.value;
+  console.log(newList);
+  document.getElementsByTagName('ul')[0].appendChild(newList);
+  document.getElementById('input-text').value="";
+  updateItems();
+  removeItem(newList);
+}
+//remove items from cart
+function removeItem(items){
+  var buttonRemove = document.createElement('button');
+  buttonRemove.innerHTML ='remove item';
+  items.appendChild(buttonRemove);
+  buttonRemove.onclick = eraseItem;
+}
+
+//erases the items from the cart on click above
+function eraseItem(event){
+  this.parentNode.remove();
+  updateItems();
+}
+
+//updates the counter above
+function updateCounter() {
   var newh2 = document.createElement('h2');
-    newh2.innerHTML = 'You have ' + itemCount.length + ' items in your shopping cart.';
-    document.getElementsByTagName('h1')[0].append(newh2);
-//alert on window- item #
-  alert('You have '+ itemCount.length + ' items in your shopping cart.');
-
-//create an id for ul tag
-  // var ulId = document.getElementsByTagName('ul');
-  // ulId.id = 'itemsInCart';
-//create an input to add items
-  // var newItem = document.createElement('input');
-  //   newItem.setAttribute('id','newItem');
-  //   newItem.setAttribute('type', 'text');
-  //   document.getElementById('div').insertBefore(newItem, null).innerHTML;
-
-function addItem(){
-  	var ul = document.getElementById("itemsInCart");
-    var newItem = document.getElementById("newItem");
-    var li = document.createElement("li");
-    li.setAttribute('id',newItem.value);
-    li.appendChild(document.createTextNode(newItem.value));
-    ul.appendChild(li);
-  }
-
-function removeItem(){
-	var ul = document.getElementById("itemsInCart");
-  var newItem = document.getElementById("newItem");
-  var item = document.getElementById(newItem.value);
-  ul.removeChild(item);
+  var listCount = document.getElementsByTagName('ul')[0].children.length;
+  newh2.innerHTML = 'You have ' + listCount + ' items in your shopping cart.';
+  document.getElementsByTagName('h1')[0].after(newh2);
+  newh2.setAttribute('id', 'counter');
+  alert('You have '+ listCount + ' items in your shopping cart.')
 }
+
+//updates text on page
+function updateItems() {
+  var counter = document.getElementById('counter');
+  var listCount = document.getElementsByTagName('ul')[0].children.length;
+  counter.innerHTML = 'You have ' + listCount + ' items in your shopping cart.';
 }
-});
