04b94f16738b4d8422dca26e8319f41203993c54
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-08-29 18:55:24 -0500

    not ready

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -6,7 +6,7 @@ window.onload = function() {
 }
 
 //create items from input text
-function insertText(){
+function insertText() {
   var addNewItems = document.createElement('input');
   addNewItems.setAttribute('type', 'text');
   addNewItems.setAttribute('placeholder', 'Type in an Item');
@@ -14,45 +14,45 @@ function insertText(){
   list[0].after(addNewItems);
   addNewItems.setAttribute('id', 'input-text');
 
-  var buttonAdd= document.createElement('button');
+  var buttonAdd = document.createElement('button');
   buttonAdd.innerHTML = 'add item';
   buttonAdd.setAttribute('id', 'addButton');
   var list1 = document.getElementsByTagName('input');
   list1[0].after(buttonAdd);
-  buttonAdd.onclick= addItem;
+  buttonAdd.onclick = addItem;
 }
 
 //add items to cart
 function addItem(event) {
   var newItemText = document.getElementById('input-text');
   var newList = document.createElement('li');
-  newList.innerHTML= newItemText.value;
+  newList.innerHTML = newItemText.value;
   console.log(newList);
   document.getElementsByTagName('ul')[0].appendChild(newList);
-  document.getElementById('input-text').value="";
+  document.getElementById('input-text').value = "";
   updateItems();
   removeItem(newList);
 }
 //remove items from cart
-function removeItem(items){
+function removeItem(items) {
   var buttonRemove = document.createElement('button');
-  buttonRemove.innerHTML ='remove item';
+  buttonRemove.innerHTML = 'remove item';
   items.appendChild(buttonRemove);
   buttonRemove.onclick = eraseItem;
 }
 
 //erases the items from the cart on click above
-function eraseItem(event){
+function eraseItem(event) {
   this.parentNode.remove();
   updateItems();
 }
 
-//erases exsisting items
-// function removeExsisting(){
-//   var buttonRemove2 = document.createElement('button');
-//   buttonRemove2.innerHTML = 'remove item';
-//   document.getElementsByTagName('ul')[0].appendChild('li');
-// }
+function removeExsisting() {
+  var buttonRemove2 = document.createElement('button');
+  buttonRemove2.innerHTML = 'remove item';
+  
+
+}
 
 // function eraseExsisting(event){
 //   this.parentNode.remove();
@@ -66,7 +66,7 @@ function updateCounter() {
   newh2.innerHTML = 'You have ' + listCount + ' items in your shopping cart.';
   document.getElementsByTagName('h1')[0].after(newh2);
   newh2.setAttribute('id', 'counter');
-  alert('You have '+ listCount + ' items in your shopping cart.')
+  alert('You have ' + listCount + ' items in your shopping cart.')
 }
 
 //updates text on page
