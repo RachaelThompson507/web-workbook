0ea8ed7c3910694cb6c1e977f62a18c6fc088a87
RachaelThompson507 <rachael.elizabeth.thompson@gmail.com>
2018-08-31 16:46:44 -0500

    tic-tac-toe

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,69 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  $(function() {
+    var turn = 'X';
+    $('[data-cell]').on('click', function() {
+      if ($(this).text() === '') {
+        $(this).text(turn);
+        checkWin();
+        if (turn === 'X') {
+          turn = 'O';
+        } else {
+          turn = 'X';
+        }
+      }
+    })
+  //win conditions
+  function checkWin() {
+      if ($('[data-cell="0"]').text() === turn &&
+        $('[data-cell="1"]').text() === turn &&
+        $('[data-cell="2"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} wins`);
+      } else if ($('[data-cell="3"]').text() === turn &&
+        $('[data-cell="4"]').text() === turn &&
+        $('[data-cell="5"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} wins`);
+      } else if ($('[data-cell="6"]').text() === turn &&
+        $('[data-cell="7"]').text() === turn &&
+        $('[data-cell="8"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} win`);
+      } else if ($('[data-cell="0"]').text() === turn &&
+        $('[data-cell="3"]').text() === turn &&
+        $('[data-cell="6"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} wins`);
+      } else if ($('[data-cell="1"]').text() === turn &&
+        $('[data-cell="4"]').text() === turn &&
+        $('[data-cell="7"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} wins`);
+      } else if ($('[data-cell="2"]').text() === turn &&
+        $('[data-cell="5"]').text() === turn &&
+        $('[data-cell="8"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} wins`);
+      } else if ($('[data-cell="0"]').text() === turn &&
+        $('[data-cell="4"]').text() === turn &&
+        $('[data-cell="8"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} wins`);
+      } else if ($('[data-cell="2"]').text() === turn &&
+        $('[data-cell="4"]').text() === turn &&
+        $('[data-cell="6"]').text() === turn)
+      {
+        $('#announce-winner').text(`${turn} wins`);
+      } else {
+        $('#announce-winner').text('CATZZZ');
+      }
+    }
+    $('#clear').click(function() {
+    $('[data-cell]').empty();
+    turn = 'X';
+    $('#announce-winner').empty();
+  })
+  })
 });
